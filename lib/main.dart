cfc4cff2c61bd9f8141304e78d5d00362acb04db
MuhammadAliKust <kustalics@gmail.com>
2021-04-01 23:19:43 +0500

    Product Screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:first_intro_class/login.dart';
+import 'package:first_intro_class/products.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
+      home: ProductScreen(),
     );
   }
 }
