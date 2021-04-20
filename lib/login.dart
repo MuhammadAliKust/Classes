24bb574113041ef7bd07fe057b66b6193c38fe15
MuhammadAliKust <kustalics@gmail.com>
2021-04-20 10:58:38 +0500

    Error Handling at Auth Module

--- lib/login.dart
+++ lib/login.dart
@@ -1,126 +1,275 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_overlay/loading_overlay.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   TextEditingController _emailController = TextEditingController();
+
   TextEditingController _pwdController = TextEditingController();
+
+  bool isLoading = false;
+
+  makeLoadingTrue() {
+    isLoading = true;
+    setState(() {});
+  }
+
+  makeLoadingFalse() {
+    isLoading = false;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 12.0),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "WELCOME",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),
-                ),
+      body: LoadingOverlay(
+        isLoading: isLoading,
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 12.0),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "SignIn to Continue",
-                  style: TextStyle(fontSize: 18, color: Colors.grey[600]),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 60,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 5),
-              child: TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                    labelText: "Email ID",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8))),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 5),
-              child: TextField(
-                obscureText: true,
-                controller: _pwdController,
-                decoration: InputDecoration(
-                    labelText: "Password",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8))),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 12.0),
-              child: Align(
-                  alignment: Alignment.topRight,
-                  child: Text("Forgot Password?")),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextButton(
-              onPressed: () {
-                FirebaseAuth.instance.createUserWithEmailAndPassword(
-                    email: _emailController.text,
-                    password: _pwdController.text);
-              },
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 58.0, vertical: 10),
-                child: Text(
-                  "Register",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-              ),
-            ),
-            TextButton.icon(
-              onPressed: () {},
-              icon: Icon(
-                Icons.eighteen_mp,
-                color: Colors.white,
-              ),
-              label: Text(
-                "Connect with Facebook",
-                style: TextStyle(color: Colors.white),
-              ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("I am new user "),
-                InkWell(
-                  onTap: () {},
+              Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: Align(
+                  alignment: Alignment.topLeft,
                   child: Text(
-                    "SignUp",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.pink),
+                    "WELCOME",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),
                   ),
                 ),
-              ],
-            )
-          ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "SignIn to Continue",
+                    style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 5),
+                child: TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                      labelText: "Email ID",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8))),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 5),
+                child: TextField(
+                  obscureText: true,
+                  controller: _pwdController,
+                  decoration: InputDecoration(
+                      labelText: "Password",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 12.0),
+                child: Align(
+                    alignment: Alignment.topRight,
+                    child: Text("Forgot Password?")),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextButton(
+                onPressed: () {
+                  _signUpUser(context);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 58.0, vertical: 10),
+                  child: Text(
+                    "Register",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  _loginUser(context);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 58.0, vertical: 10),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green),
+                ),
+              ),
+              TextButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.eighteen_mp,
+                  color: Colors.white,
+                ),
+                label: Text(
+                  "Connect with Facebook",
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("I am new user "),
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      "SignUp",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.pink),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  _signUpUser(BuildContext context) async {
+    makeLoadingTrue();
+    try {
+      ///This will allow user to register in firebase
+      return await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: _emailController.text, password: _pwdController.text)
+          .whenComplete(() => makeLoadingFalse())
+          .then((value) {
+        return showDialog<void>(
+          context: context,
+          barrierDismissible: false,
+          // false = user must tap button, true = tap outside dialog
+          builder: (BuildContext dialogContext) {
+            return AlertDialog(
+              title: Text('Message!'),
+              content: Text("Registered successfully"),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Okay'),
+                  onPressed: () {
+                    Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      });
+    } on FirebaseAuthException catch (e) {
+      makeLoadingFalse();
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+        // false = user must tap button, true = tap outside dialog
+        builder: (BuildContext dialogContext) {
+          return AlertDialog(
+            title: Text('ALert!'),
+            content: Text(e.message),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Okay'),
+                onPressed: () {
+                  Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  _loginUser(BuildContext context) async {
+    makeLoadingTrue();
+    try {
+      ///This will allow user to register in firebase
+      return await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: _emailController.text, password: _pwdController.text)
+          .whenComplete(() => makeLoadingFalse())
+          .then((value) {
+        return showDialog<void>(
+          context: context,
+          barrierDismissible: false,
+          // false = user must tap button, true = tap outside dialog
+          builder: (BuildContext dialogContext) {
+            return AlertDialog(
+              title: Text('Message!'),
+              content: Text("LoggedIn successfully"),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Okay'),
+                  onPressed: () {
+                    Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      });
+      ;
+    } on FirebaseAuthException catch (e) {
+      makeLoadingFalse();
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+        // false = user must tap button, true = tap outside dialog
+        builder: (BuildContext dialogContext) {
+          return AlertDialog(
+            title: Text('ALert!'),
+            content: Text(e.message),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Okay'),
+                onPressed: () {
+                  Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 }
